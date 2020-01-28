9af6b236ce92a92f845c788150a8df53730c8d40
Artur <artur.marciniak94@gmail.com>
2020-01-28 16:59:51 +0100

    Added test of changing currently picked color of guess

--- Tests/test_codebreaker.py
+++ Tests/test_codebreaker.py
@@ -4,6 +4,8 @@ from codebreaker import Codebreaker
 
 
 class TestCodebreaker(unittest.TestCase):
+
+
     def test_CodebreakerGetsPoint(self):
         player = Codebreaker()
         self.assertEqual(player.points, 0)
@@ -33,3 +35,24 @@ class TestCodebreaker(unittest.TestCase):
         player.change_fourth_color(2)
         self.assertEqual(player.guess, [None, None, 0, 2])
         self.assertFalse(player.confirm_guess())
+
+    def test_CodebreakerChangesColorOfHisGuess(self):
+        player = Codebreaker()
+        player.change_first_color(0)
+        player.change_second_color(0)
+        player.change_third_color(0)
+        player.change_fourth_color(0)
+        player.change_first_color(2)
+        player.change_fourth_color(2)
+        self.assertEqual(player.guess, [2, 0, 0, 2])
+
+    def test_CodebreakerResetsGuess(self):
+        player = Codebreaker()
+        player.change_first_color(1)
+        player.change_second_color(2)
+        player.change_third_color(3)
+        player.change_fourth_color(4)
+        self.assertEqual(player.guess, [1, 2, 3, 4])
+        player.reset_guess()
+        self.assertEqual(player.guess, [None, None, None, None])
+
