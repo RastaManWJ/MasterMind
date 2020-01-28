c39d1e455085a1cb14c6e9f3951189f34172a04d
Artur <artur.marciniak94@gmail.com>
2020-01-28 17:01:02 +0100

    Added tests of several cases of providing feedback by Codemaker

--- Tests/test_codemaker.py
+++ Tests/test_codemaker.py
@@ -14,6 +14,40 @@ class TestCodemaker(unittest.TestCase):
         ai.draw_code()
         self.assertNotEqual(ai.code, [None]*4)
 
-    def test_CodemakerProvidedFeedback(self):
-        pass
+    def test_CodemakerProvidedFeedbackOfAllWrongPegs(self):
+        ai = Codemaker()
+        ai.code = [0, 1, 2, 3]
+        guess = [4, 4, 4, 4]
+        feedback = ai.provide_feedback(guess)
+        self.assertEqual([0, 0, 0, 0], feedback)
 
+    def test_CodemakerProvidedFeedbackOfTwoCorrectIdealPlacements(self):
+        ai = Codemaker()
+        ai.code = [0, 1, 2, 3]
+        guess = [0, 1, 4, 4]
+        feedback = ai.provide_feedback(guess)
+        self.assertEqual([2, 2, 0, 0], feedback)
+
+    def test_CodemakerProvidedFeedbackOfTwoMatchingButWrongPlacedColors(self):
+        ai = Codemaker()
+        ai.code = [0, 1, 2, 3]
+        guess = [1, 0, 4, 4]
+        feedback = ai.provide_feedback(guess)
+        self.assertEqual([1, 1, 0, 0], feedback)
+
+    def test_CodemakerProvidedFeedbackOfTwoCorrectAndOneWrongPlacedColors(self):
+        ai = Codemaker()
+        ai.code = [0, 1, 2, 3]
+        guess = [1, 4, 2, 3]
+        feedback = ai.provide_feedback(guess)
+        self.assertEqual([2, 2, 1, 0], feedback)
+
+    def test_FeedbackResets(self):
+        ai = Codemaker()
+        ai.code = [0, 1, 2, 3]
+        guess = [1, 4, 2, 3]
+        feedback = ai.provide_feedback(guess)
+        self.assertEqual([2, 2, 1, 0], feedback)
+        ai.reset_feedback_pegs()
+        self.assertEqual(ai.key_pegs, [0]*4)
+        self.assertEqual(ai.key_peg_amount, 0)
